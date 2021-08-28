90ccbd6028e7c238a65bb9a8a46416a4f9f2daa0
steve <seasonsv@gmail.com>
2021-08-28 14:42:46 +0800

    tlc20

--- CoinKit/Classes/Models/CoinType.swift
+++ CoinKit/Classes/Models/CoinType.swift
@@ -10,7 +10,7 @@ public enum CoinType {
     case binanceSmartChain
     case erc20(address: String)
     case trc20(address: String)
-    case drc20(address: String)
+    case tlc20(address: String)
     case bep2(symbol: String)
     case bep20(address: String)
     case unsupported(id: String)
@@ -34,7 +34,7 @@ extension CoinType: Equatable {
             return lhsAddress.lowercased() == rhsAddress.lowercased()
         case (.trc20(let lhsAddress), .trc20(let rhsAddress)):
             return lhsAddress.lowercased() == rhsAddress.lowercased()
-        case (.drc20(let lhsAddress), .drc20(let rhsAddress)):
+        case (.tlc20(let lhsAddress), .tlc20(let rhsAddress)):
             return lhsAddress.lowercased() == rhsAddress.lowercased()
         case (.bep2(let lhsSymbol), .bep2(let rhsSymbol)):
             return lhsSymbol == rhsSymbol
@@ -80,7 +80,7 @@ extension CoinType: Identifiable {
             switch chunks[0] {
             case "erc20": self = .erc20(address: String(chunks[1]))
             case "trc20": self = .trc20(address: String(chunks[1]))
-            case "drc20": self = .drc20(address: String(chunks[1]))
+            case "tlc20": self = .tlc20(address: String(chunks[1]))
             case "bep2": self = .bep2(symbol: String(chunks[1]))
             case "bep20": self = .bep20(address: String(chunks[1]))
             case "unsupported": self = .unsupported(id: chunks.suffix(from: 1).joined(separator: "|"))
@@ -101,7 +101,7 @@ extension CoinType: Identifiable {
         case .tl: return "tl"
         case .erc20(let address): return ["erc20", address].joined(separator: "|")
         case .trc20(let address): return ["trc20", address].joined(separator: "|")
-        case .drc20(let address): return ["drc20", address].joined(separator: "|")
+        case .tlc20(let address): return ["tlc20", address].joined(separator: "|")
         case .binanceSmartChain: return "binanceSmartChain"
         case .bep2(let symbol): return ["bep2", symbol].joined(separator: "|")
         case .bep20(let address): return ["bep20", address].joined(separator: "|")
